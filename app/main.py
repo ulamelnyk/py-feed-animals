4ecccc28257bdbfbd0a18a9222b363dc33184192
ulianamelnyk <melnykula23@gmail.com>
2026-05-02 08:32:51 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,73 @@
-#  write your code here
+class Animal:
+    def __init__(self, name, appetite, is_hungry=True):
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self):
+        print(f'Hello, I\'m {self.name}')
+
+    def feed(self):
+        if self.is_hungry:
+            print(f'Eating {self.appetite} food points...')
+            self.is_hungry = False
+            return self.appetite
+        return 0
+
+class Cat(Animal):
+    def __init__(self, name, is_hungry=True):
+        super().__init__(name, 3)
+        self.is_hungry = is_hungry
+
+    @staticmethod
+    def catch_mouse():
+        print(f'The hunt began!')
+
+class Dog(Animal):
+    def __init__(self, name, is_hungry=True):
+        super().__init__(name, 7)
+        self.is_hungry = is_hungry
+
+    @staticmethod
+    def bring_slippers():
+        print(f'The slippers delivered!')
+
+def feed_animals(lista):
+    total = 0
+
+    for animal in lista:
+        total += animal.feed()
+
+    return total
+
+print('LION')
+lion = Animal("Lion", 25)
+lion.print_name()  # "Hello, I'm Lion"
+food_points = lion.feed()  # "Eating 25 food points..."
+print(food_points)  # 25
+print(lion.is_hungry)  # False
+print(lion.feed())  # 0
+
+print('\nCAT')
+cat = Cat("Cat")
+cat.print_name()  # "Hello, I'm Cat"
+cat.feed()  # "Eating 3 food points"
+
+cat2 = Cat("Cat", False)
+print(cat2.feed())  # 0
+cat2.catch_mouse()  # "The hunt began!"
+
+print('\nDOG')
+dog = Dog("Dog")
+dog.print_name()  # "Hello, I'm Dog"
+dog.feed()  # "Eating 7 food points"
+
+dog2 = Dog("Dog", False)
+print(dog2.feed())  # 0
+dog2.bring_slippers()  # "The slippers delivered!"
+
+print('\nANIMALS')
+cat = Cat("Cat", False)
+lion = Animal("Lion", 25, True)
+dog = Dog("Dog")
+feed_animals([cat, lion, dog]) == 32
